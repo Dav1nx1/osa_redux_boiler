dd4b11c2e0852dffa14ff8115cc1f1c8338a6735
Oscar Corcho <ojc8818@gmail.com>
2017-09-12 11:08:34 -0400

    Init Commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,19 @@
-import { combineReducers } from "redux";
-import { routerReducer } from "react-router-redux";
-import { reducer as formReducer } from "redux-form";
+import { combineReducers } from 'redux'
+import { HELLO_WORLD } from '../actions/index'
 
-// main reducers
-export const reducers = combineReducers({
-  routing: routerReducer,
-  form: formReducer,
-  // your reducer here
-});
+
+function helloWorld(state = { message: 'Hello' }, action) {
+  switch (action.type) {
+    case HELLO_WORLD:
+      console.log('reducer: helloWorld')
+      return Object.assign({}, state, { message: 'Hello, World!' })
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  helloWorld
+})
+
+export default rootReducer
