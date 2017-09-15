e21b206aa55b35870a10690cdead1bc54b7fb9d4
Oscar Corcho <ojc8818@gmail.com>
2017-09-15 09:06:20 -0400

    ajustes en la navegacion.

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import configureStore from '../stores/index'
-import AppContainer from './AppContainer';
+import NavContainer from './navContainer';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import HelloWorld from './HelloWorld';
@@ -12,11 +12,10 @@ const store = configureStore()
 
 class Root extends Component {
   render() {
-    console.log(this.props)
     return (
       <Provider store={this.props.store}>
         <Router history={this.props.history}>
-          <Route path="/" component={ AppContainer } >
+          <Route path="/" component={ NavContainer } history={this.props.history} >
             <IndexRoute component= { SmartDash } />
             <Route path="dashboard" component={ HelloWorld } />
           </Route>
@@ -30,7 +29,5 @@ Root.propTypes = {
   store: PropTypes.object.isRequired,
   history: PropTypes.object.isRequired
 }
-
-
 
 export default Root
